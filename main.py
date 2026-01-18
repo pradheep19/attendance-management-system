2c1312f5fa9e4512979718487bb22ff20f8bb65d
Pradheep <pradheep.r2023@vitstudent.ac.in>
2026-01-18 11:56:45 +0530

    Implemented attendance marking feature

--- main.py
+++ main.py
@@ -15,3 +15,29 @@ class AttendanceSystem:
 system = AttendanceSystem()
 system.add_student("101", "Rahul", "CSE")
 print(system.list_students())
+
+#attendance
+
+from datetime import date
+
+class AttendanceSystem:
+    def __init__(self):
+        self.students = {}
+        self.attendance = {}
+
+    def add_student(self, student_id, name, department):
+        self.students[student_id] = {
+            "name": name,
+            "department": department
+        }
+
+    def mark_attendance(self, student_id, status):
+        today = str(date.today())
+        if student_id not in self.attendance:
+            self.attendance[student_id] = {}
+        self.attendance[student_id][today] = status
+
+system = AttendanceSystem()
+system.add_student("101", "Rahul", "CSE")
+system.mark_attendance("101", "Present")
+print(system.attendance)
